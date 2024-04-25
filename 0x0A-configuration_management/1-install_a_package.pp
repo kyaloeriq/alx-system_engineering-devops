46e5699e9aeb15e301f40cf7b4b8a31b19f9bf7a
kyaloeriq <kyaloeriq@gmail.com>
2024-04-25 12:41:51 -0700

    Using Puppet to install flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,4 +1,10 @@
 # Using Puppet to install flask from pip3
+
+package { 'flask':
+  ensure   => absent,
+  provider => 'pip3',
+}
+
 package { 'flask':
   ensure   => '2.1.0',
   provider => 'pip3',
