f25b9ec5445e13ec98df815e5c909fff11409fce
kyaloeriq <kyaloeriq@gmail.com>
2024-06-05 08:23:12 -0700

    function that queries the Reddit API and returns the number of subscribers

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,10 +10,7 @@ def number_of_subscribers(subreddit):
     Queries Reddit API for the number of subscribers for a given subreddit
     """
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {
-            'User-Agent':
-            'python:subreddit.subscriber.counter:v1.0 (by /u/yourusername)'
-            }
+    headers = {'User-Agent': 'python:subreddit.subscriber.counter:v1.0 (by /u/yourusername)'}
 
     try:
         response = requests.get(url, headers=headers, allow_redirects=False)
