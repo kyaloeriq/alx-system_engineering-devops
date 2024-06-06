af2d53e95d5a56893c4626a4c01c82777ecfb77e
kyaloeriq <kyaloeriq@gmail.com>
2024-06-06 07:14:49 -0700

    fix it and then automate it using Puppet

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -26,25 +26,3 @@ file { '/var/www/html/index.html':
   mode    => '0644',
   require => File['/var/www/html'],
 }
-
-# Ensure the Apache configuration file exists and is correct
-file { '/etc/apache2/sites-available/000-default.conf':
-  ensure  => file,
-  content => template('apache2/000-default.conf.erb'),
-  notify  => Service['apache2'],
-}
-
-# Ensure the site is enabled
-exec { 'enable_site':
-  command => '/usr/sbin/a2ensite 000-default.conf',
-  unless  => '/usr/sbin/a2query -s 000-default.conf',
-  notify  => Service['apache2'],
-}
-
-# Ensure Apache has correct permissions to its log directory
-file { '/var/log/apache2':
-  ensure => directory,
-  owner  => 'root',
-  group  => 'adm',
-  mode   => '0755',
-}
