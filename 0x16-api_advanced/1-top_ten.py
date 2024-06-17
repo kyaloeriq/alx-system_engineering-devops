c13212432957df1ecb9d9b823fa2768cd350c5e6
kyaloeriq <kyaloeriq@gmail.com>
2024-06-16 22:52:54 -0700

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -19,16 +19,19 @@ def top_ten(subreddit):
         response = requests.get(url, headers=headers, allow_redirects=False)
         response.raise_for_status()
 
-        if response.status_code == 200:
+        try:
             data = response.json()
-            if 'data' in data and 'children' in data['data']:
-                posts = data['data']['children']
-                for post in posts:
-                    print(post['data']['title'])
-            else:
-                print(None)
+        except ValueError:
+            print(None)
+            return
+
+        if 'data' in data and 'children' in data['data']:
+            posts = data['data']['children']
+            for post in posts:
+                print(post['data']['title'])
         else:
             print(None)
+
     except requests.exceptions.HTTPError:
         print(None)
     except requests.exceptions.ConnectionError:
