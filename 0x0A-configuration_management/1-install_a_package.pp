29bbc4483557a87ae93d3aaa215b8322514a47e0
kyaloeriq <kyaloeriq@gmail.com>
2024-06-24 11:43:50 -0700

    installs flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -4,9 +4,3 @@ package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
-# Add environment path for flask executable
-file { '/etc/profile.d/flask.sh':
-  ensure   => present,
-  content  => "export PATH=$PATH:/usr/local/bin\n",
-  mode     => '0644',
-}
