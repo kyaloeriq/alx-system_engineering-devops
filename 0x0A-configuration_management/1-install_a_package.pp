bccb969397138240031a0cea2573f1679ff43f71
kyaloeriq <kyaloeriq@gmail.com>
2024-06-24 11:29:28 -0700

    installs flask from pip3

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,11 +1,6 @@
 # Using Puppet to install flask from pip3
 
-package { 'flask':
-  ensure   => absent,
-  provider => 'pip3',
-}
-
-package { 'flask':
+package { 'Flask':
   ensure   => '2.1.0',
   provider => 'pip3',
 }
